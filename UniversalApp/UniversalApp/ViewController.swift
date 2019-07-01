7ca5fba2c5fb383aee1cd653e930c478792cbc80
Hngfu <djsl1018@naver.com>
2019-07-01 16:49:15 +0900

    [ViewController] UI component들을 연결

--- UniversalApp/UniversalApp/ViewController.swift
+++ UniversalApp/UniversalApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var actorImageView: UIImageView!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var birthdayButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        actorImageView.image = UIImage(named: "Name".localized)
+        languageLabel.text = "Language".localized
+        nameLabel.text = "Name".localized
+        infoLabel.text = "Info".localized
+        birthdayButton.titleLabel?.text = "Birthday".localized
     }
-
-
 }
 
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
