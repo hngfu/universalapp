d388b6cbbfce5841be8dbc4a401fb1e7b90bbb3e
Hngfu <djsl1018@naver.com>
2019-07-01 13:59:27 +0900

    Project 생성

---
+++ UniversalApp/UniversalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversalApp
+//
+//  Created by 조재흥 on 19. 7. 1..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
