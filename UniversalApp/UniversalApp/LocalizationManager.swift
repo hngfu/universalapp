4243c829745e33b2acc0ccab066e60d0fcc58cf9
Hngfu <djsl1018@naver.com>
2019-07-02 21:49:21 +0900

    LocalizationManager 추가, string File key에 language 추가

---
+++ UniversalApp/UniversalApp/LocalizationManager.swift
@@ -0,0 +1,15 @@
+//
+//  LocalizationManager.swift
+//  UniversalApp
+//
+//  Created by 조재흥 on 19. 7. 2..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+struct LocalizationManager {
+    private let local = Locale.current
+    
+    
+}
